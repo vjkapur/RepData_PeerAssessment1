1bd2f4283fde5e070d5eb407e96ec290d8355d63
VJ <vj@vjkapur.com>
2014-07-19 13:59:54 -0400

    some documentation

--- updatepush.sh
+++ updatepush.sh
@@ -1,5 +1,8 @@
+# This script compiles the assignment Rmd file into both markdown and html, and adds/commits/remote-pushes the results
+# The script takes one parameter: a comment for the commit
+# usage: ./updatepush.sh "this a commit message"
 Rscript -e "library(knitr); knit('PA1_template.Rmd'); knit2html('PA1_template.Rmd')"
 git add PA1_*
-git add figure
+git add figure/*
 git commit -m "$1"
 git push origin master
